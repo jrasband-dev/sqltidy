e7e1bbf4b4fd1582c1a94aa32c5ac7edbf9ff2ed
jrasband-dev <jayden.rasband@gmail.com>
2025-12-29 23:14:19 -0700

    Bumped Version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,7 +16,7 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 project = 'SQLTidy'
 copyright = '2025, Jayden Rasband'
 author = 'Jayden Rasband'
-release = '0.1.0'
+release = '0.2.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
